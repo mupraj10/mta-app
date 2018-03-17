26a20d71378c70dd73a34a1edde91988937b024b
Maggy Prajapati <mupraj10@gmail.com>
2018-03-17 19:31:21 -0400

    first one

---
+++ app/config/constants.js
@@ -0,0 +1,8 @@
+export const FIREBASE_API_KEY = 'AIzaSyDwvIIQMcJacdI_Hj5EmEtGlduxZb_C5Ck';
+export const FIREBASE_AUTH_DOMAIN = 'mta-app-6fc3f.firebaseapp.com';
+export const FIREBASE_DATABASE_URL = 'https://mta-app-6fc3f.firebaseio.com';
+export const FIREBASE_PROJECT_ID = 'mta-app-6fc3f';
+export const FIREBASE_STORAGE_BUCKET = 'mta-app-6fc3f.appspot.com';
+export const FIREBASE_MESSAGING_SENDER_ID = '217657983730';
+
+// export const FACEBOOK_APP_ID = <YOUR_FACEBOOK_APP_ID>;
